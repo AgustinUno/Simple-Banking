4044e51ae2b00fbe5dce57efd57283d1537b85ad
Justine <118758009+AgustinUno@users.noreply.github.com>
2023-11-17 23:40:01 +0800

    FIle DIv

--- MAIN-ROUTINE.cob
+++ MAIN-ROUTINE.cob
@@ -1,53 +1,39 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID.      Basic-Banking.
-       AUTHOR.          AgustinUno
+           PROGRAM-ID.      Basic-Banking.
+           AUTHOR.          AgustinUno
+      *-----------------------------------------------------------------
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       SELECT INFILE
 
+           ASSIGN TO 'Simple Banking/DATA/Student-Data.txt'
+               ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT OUTFILE
+
+           ASSIGN TO 'Simple Banking/DATA/Output-Data.txt'
+               ORGANIZATION IS LINE SEQUENTIAL.
+      *-----------------------------------------------------------------
        DATA DIVISION.
        FILE SECTION.
-       FD INFILE
-           LABEL RECORDS ARE OMITTED
-           RECORDING MODE F
-           RECORD CONTAINS 40 CHARACTERS
-           DATA RECORD IS IN-RECORD.
+       FD INFILE.
+       01  IN-RECORD.
+           05 IN-STD-ID PIC 9(4).
+           05 IN-STD-NAME PIC X(20).
 
-       01 IN-RECORD.
-          05 IN-STUDENT-CODE  PIC 9(4).
-          05 FILLER             PIC X(11).
-          05 IN-STUDENT-NAME  PIC X(25).
-
-       FD OUTFILE
-           LABEL RECORDS ARE OMITTED
-           RECORDING MODE F
-           RECORD CONTAINS 40 CHARACTERS
-           DATA RECORD IS OUT-RECORD.
-
-       01 OUT-RECORD.
-          05 OUT-STUDENT-CODE  PIC 9(4).
-          05 FILLER              PIC X(11).
-          05 OUT-STUDENT-NAME  PIC X(25).
+       FD OUTFILE.
+       01  OUT-RECORD.
+           05 OUT-STD-ID PIC 9(4).
+           05 OUT-STD-NAME PIC X(20).
 
        WORKING-STORAGE SECTION.
-       01 Counter PIC 9(3) VALUE 0.
-
+       01 SCTR PIC 9(3) VALUE 0.
+       01 EOF-SWITCH PIC A(1) VALUE 'N'.
+      *-----------------------------------------------------------------
        PROCEDURE DIVISION.
            OPEN INPUT INFILE
            OPEN OUTPUT OUTFILE.
 
-           PERFORM UNTIL Counter > 10
-               READ INFILE
-               AT END
-                   EXIT PERFORM
-               END-READ.
 
-               MOVE IN-STUDENT-CODE TO OUT-STUDENT-CODE
-               MOVE IN-STUDENT-NAME TO OUT-STUDENT-NAME
-               WRITE OUT-RECORD.
-               ADD 1 TO Counter.
-
-           END-PERFORM.     
-
-           CLOSE INFILE
-           CLOSE OUTFILE
-          
            STOP RUN.
 
