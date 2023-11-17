f9e2105d121be5f8bfb53b2449ea01f6f944d427
Justine <118758009+AgustinUno@users.noreply.github.com>
2023-11-18 02:13:16 +0800

    Write

--- MAIN-ROUTINE.cob
+++ MAIN-ROUTINE.cob
@@ -6,9 +6,9 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
                 SELECT INFILE
-       ASSIGN TO "/home/lloyd/Desktop/Code/Student-Data.txt" 
+       ASSIGN TO "/home/lloyd/Desktop/Code/Student-Data.txt"
            ORGANIZATION IS LINE SEQUENTIAL.
-       
+
                 SELECT OUTFILE
        ASSIGN TO "/home/lloyd/Desktop/Code/Output-Data.txt"
            ORGANIZATION IS LINE SEQUENTIAL.
@@ -24,7 +24,7 @@
            05 IN-CODE PIC A(1).
            05 IN-STD-CASH PIC 9(5)V99.
       *     05 IN-STD-CASH PIC 9(5)V99.
-      
+
        FD OUTFILE.
        01  OUT-RECORD.
            05 OUT-STD-ID PIC 9(4).
@@ -34,7 +34,7 @@
        01 SCTR PIC 9(3) VALUE 0.
        01 EOF-SWITCH PIC A(1) VALUE 'N'.
        01 FRMT-VAL PIC $$$$,$$$,$$9.99.
-      
+
       *-----------------------------------------------------------------
        PROCEDURE DIVISION.
          MAIN-ROUTINE.
@@ -43,21 +43,25 @@
 
            PERFORM READ-PROCEDURE.
 
+
+
            CLOSE INFILE
            CLOSE OUTFILE.
 
            STOP RUN.
 
        READ-PROCEDURE.
-           
+
            PERFORM UNTIL EOF-SWITCH = 'Y'
                READ INFILE
                    AT END
                        MOVE 'Y' TO EOF-SWITCH
                    NOT AT END
+									      MOVE IN-STD-ID TO OUT-STD-ID
                         MOVE IN-STD-CASH TO FRMT-VAL
                        DISPLAY IN-STD-ID, "  ", IN-STD-NAME
-                                 ,"  ", FRMT-VAL       
+                                 ,"  ", FRMT-VAL
+											WRITE OUT-RECORD
                END-READ
            END-PERFORM.
-     
+              
