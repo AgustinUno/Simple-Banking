1aae9157d4b97bd3f115f8104a5611f547bcd353
Justine <118758009+AgustinUno@users.noreply.github.com>
2023-11-17 22:04:36 +0800

    Update MAIN-ROUTINE.cob

--- MAIN-ROUTINE.cob
+++ MAIN-ROUTINE.cob
@@ -1,45 +1,53 @@
-      *------------------------
-      * logically based on lecture example by Ms. Nayre
-      * BSIT 2-2
-      *------------------------
        IDENTIFICATION DIVISION.
-      *------------------------
-       PROGRAM-ID.      Rural-Banking-01.
-       AUTHOR.          Agustin
+       PROGRAM-ID.      Basic-Banking.
+       AUTHOR.          AgustinUno
 
-      *--------------
        DATA DIVISION.
-      *--------------
        FILE SECTION.
-        FD INFILE
+       FD INFILE
            LABEL RECORDS ARE OMITTED
-           RECORDING MODE F  /* Fixed recording mode */
-           RECORD CONTAINS 80 CHARACTERS
+           RECORDING MODE F
+           RECORD CONTAINS 40 CHARACTERS
            DATA RECORD IS IN-RECORD.
 
-       01 IN-RECORD PIC X(80).
+       01 IN-RECORD.
+          05 IN-STUDENT-CODE  PIC 9(4).
+          05 FILLER             PIC X(11).
+          05 IN-STUDENT-NAME  PIC X(25).
 
        FD OUTFILE
            LABEL RECORDS ARE OMITTED
-           RECORDING MODE F  /* Fixed recording mode */
-           RECORD CONTAINS 80 CHARACTERS
+           RECORDING MODE F
+           RECORD CONTAINS 40 CHARACTERS
            DATA RECORD IS OUT-RECORD.
 
-       01 OUT-RECORD PIC X(80).
+       01 OUT-RECORD.
+          05 OUT-STUDENT-CODE  PIC 9(4).
+          05 FILLER              PIC X(11).
+          05 OUT-STUDENT-NAME  PIC X(25).
 
        WORKING-STORAGE SECTION.
-       01  Counter           PIC 9(3) VALUE 0.
+       01 Counter PIC 9(3) VALUE 0.
 
        PROCEDURE DIVISION.
-      * Main Procedure
-           READ PROCEDURE.
+           OPEN INPUT INFILE
+           OPEN OUTPUT OUTFILE.
 
-           DISPLAY 'Hello, World!'.
+           PERFORM UNTIL Counter > 10
+               READ INFILE
+               AT END
+                   EXIT PERFORM
+               END-READ.
 
-      * Perform a loop to display numbers 1 to 10.
-           PERFORM VARYING Counter FROM 1 BY 1 UNTIL Counter > 10
-               DISPLAY 'Counter is ' Counter
-           END-PERFORM.
+               MOVE IN-STUDENT-CODE TO OUT-STUDENT-CODE
+               MOVE IN-STUDENT-NAME TO OUT-STUDENT-NAME
+               WRITE OUT-RECORD.
+               ADD 1 TO Counter.
 
-      * End the program.
+           END-PERFORM.     
+
+           CLOSE INFILE
+           CLOSE OUTFILE
+          
            STOP RUN.
+
