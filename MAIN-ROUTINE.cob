6db03bc489ca1b69d7a7b3c20fd89e00010c234d
Justine <118758009+AgustinUno@users.noreply.github.com>
2023-11-18 15:35:13 +0800

    Read

--- MAIN-ROUTINE.cob
+++ MAIN-ROUTINE.cob
@@ -5,33 +5,34 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-                SELECT INFILE
+                SELECT INFILE        
        ASSIGN TO "/home/lloyd/Desktop/Code/Student-Data.txt".
 
 
                 SELECT OUTFILE
-       ASSIGN TO "/home/lloyd/Desktop/Code/Output-Data.txt".
+       ASSIGN TO "/home/lloyd/Desktop/Code/Output-Data.txt"
+                ACCESS MODE IS SEQUENTIAL.     *> BY LINE, FROM START 
 
       *-----------------------------------------------------------------
        DATA DIVISION.
        FILE SECTION.
        FD INFILE RECORDING MODE F.
        01  IN-RECORD.
-           05 IN-STD-ID PIC 9(4).
-           05 SPC-01 PIC X(12).
-           05 IN-STD-NAME PIC X(20).
-           05 SPC-02 PIC X(4).
+           05 IN-STD-ID PIC 9(8).
+      *    05 SPC-01 PIC X(12).
+           05 IN-STD-NAME PIC X(24).
+      *    05 SPC-02 PIC X(4).
            05 IN-CODE PIC A(1).
-           05 IN-STD-CASH PIC 9(5)V99.
+           05 IN-STD-CASH PIC 9(6).
 
-       FD OUTFILE RECORDING MODE F.
+       FD OUTFILE RECORDING MODE F.    *> F = FIXED LENGTH 
        01  OUT-RECORD.
-           05 OUT-STD-ID PIC 9(4).
-           05 SPC-011 PIC X(12).
-           05 OUT-STD-NAME PIC X(20).
-           05 SPC-022 PIC X(4) .
+           05 OUT-STD-ID PIC 9(8).
+      *    05 SPC-011 PIC X(12).
+           05 OUT-STD-NAME PIC X(24).
+      *    05 SPC-022 PIC X(4) .
            05 OUT-CODE PIC A(1).
-           05 OUT-STD-CASH PIC 9(5)V99.
+           05 OUT-STD-CASH PIC 9(6).
 
        WORKING-STORAGE SECTION.
        01 SCTR PIC 9(3) VALUE 0.
@@ -61,6 +62,8 @@
                             MOVE IN-STD-CASH TO OUT-STD-CASH
                             MOVE IN-STD-NAME TO OUT-STD-NAME
                             MOVE IN-CODE TO OUT-CODE
-                            WRITE OUT-RECORD FROM IN-RECORD
+                            WRITE OUT-RECORD FROM IN-RECORD  
                END-READ
            END-PERFORM.
+                           DISPLAY "TRANSFER SUCCESS".  *>DEBUGGER 
+                           
